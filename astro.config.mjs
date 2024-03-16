7da775a31c07c05a29ddda76bdeff6065e0ec6b7
SHOMRIDDHO'S WORLD <shomriddho.world@gmail.com>
2024-03-16 22:56:38 +0600

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://paruli.netlify.app/',
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+
 });
